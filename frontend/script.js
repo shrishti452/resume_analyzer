8a255247f706f8e411186c48f016e1c0657778d8
Shrishti <shrishtiagarwal205@gmail.com>
2026-06-25 17:08:36 +0530

    Implement resume upload and file storage

--- frontend/script.js
+++ frontend/script.js
@@ -11,14 +11,34 @@ button.addEventListener("click", async () => {
     output.innerText = data.message;
 
 });
+
 const fileInput = document.getElementById("resumeFile");
 const uploadBtn = document.getElementById("uploadBtn");
 const uploadStatus = document.getElementById("uploadStatus");
 
-uploadBtn.addEventListener("click", () => {
+uploadBtn.addEventListener("click", async () => {
 
     const file = fileInput.files[0];
 
-    console.log(file);
+    if (!file) {
+        uploadStatus.innerText = "Please select a file";
+        return;
+    }
+
+    const formData = new FormData();
+
+    formData.append("resume", file);
+
+    const response = await fetch(
+        "http://127.0.0.1:5000/upload",
+        {
+            method: "POST",
+            body: formData
+        }
+    );
+
+    const data = await response.json();
+
+    uploadStatus.innerText = data.message;
 
 });
